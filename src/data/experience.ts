38b11153830bcde9f2b067786cb195c86035db6d
Joshua's Laptop <norcaz919@gmail.com>
2026-04-27 15:08:47 -0700

    Exportable experience data used for experience component

---
+++ src/data/experience.ts
@@ -0,0 +1,22 @@
+import type { Experience } from "./types";
+
+export const experiences: Experience[] = [
+  {
+    role: "Incoming SWE Intern",
+    company: "Raytheon",
+    dateRange: "Jun 2026 – Aug 2026",
+    description:
+      "Incoming Test Equipment intern at Raytheon, designing, developing, and implementing software to support the testing of advanced defense systems.",
+    logoSrc: "/images/logos/raytheon_logo.svg",
+    logoAlt: "Raytheon",
+  },
+  {
+    role: "Software Engineer Intern",
+    company: "Cita Marketplace",
+    dateRange: "June 2025 – August 2025",
+    description:
+      "Developed and deployed a feature for sellers to submit restaurant reservations, ensuring data validation and seamless integration using Next.js, TRPC, and SQL",
+    logoSrc: "/images/logos/cita_marketplace_logo.svg",
+    logoAlt: "Cita Marketplace",
+  },
+];
